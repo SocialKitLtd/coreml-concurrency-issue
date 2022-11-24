aff61e6a1f6bef9085940d1b3ade4756eb783c21
Roi Mulia <roi@socialkit.com>
2022-11-24 14:28:39 +0200

    Added files

--- coreml-concurrency-issue/AppDelegate.swift
+++ coreml-concurrency-issue/AppDelegate.swift
@@ -34,3 +34,34 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 }
 
+
+@_transparent @discardableResult public func measure(label: String? = nil, tests: Int = 1, printResults output: Bool = true, setup: @escaping () -> Void = { return }, _ block: @escaping () -> Void) -> Double {
+    
+    guard tests > 0 else { fatalError("Number of tests must be greater than 0") }
+    
+    var avgExecutionTime : CFAbsoluteTime = 0
+    for _ in 1...tests {
+        setup()
+        let start = CFAbsoluteTimeGetCurrent()
+        block()
+        let end = CFAbsoluteTimeGetCurrent()
+        avgExecutionTime += end - start
+    }
+    
+    avgExecutionTime /= CFAbsoluteTime(tests)
+    
+    if output {
+        let avgTimeStr = "\(avgExecutionTime)".replacingOccurrences(of: "e|E", with: " × 10^", options: .regularExpression, range: nil)
+        
+        if let label = label {
+            print(label, "▿")
+            print("\tExecution time: \(avgTimeStr)s")
+            print("\tNumber of tests: \(tests)\n")
+        } else {
+            print("Execution time: \(avgTimeStr)s")
+            print("Number of tests: \(tests)\n")
+        }
+    }
+    
+    return avgExecutionTime
+}
--- coreml-concurrency-issue/ViewController.swift
+++ coreml-concurrency-issue/ViewController.swift
@@ -6,14 +6,34 @@
 //
 
 import UIKit
+import CoreML
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let parallelTaskCount = 3
+        
+        for i in 0...parallelTaskCount - 1 {
+            DispatchQueue.global(qos: .userInteractive).async {
+                let image = UIImage(named: "image.jpg")!
+                self.runPrediction(index: i, image: image, shouldAddContuter: true)
+            }
+        }
     }
 
-
+    
+    func runPrediction(index: Int, image: UIImage, shouldAddContuter: Bool = false) {
+        let conf = MLModelConfiguration()
+        conf.computeUnits = .cpuAndGPU
+        conf.allowLowPrecisionAccumulationOnGPU = true
+        
+        let myModel = try! MyModel(configuration: conf)
+        let myModelInput = try! MyModelInput(LR_inputWith: image.cgImage!)
+        // Prediction
+        let predicition = try! myModel.prediction(input: myModelInput)
+        print("finished proccessing \(index)")
+    }
+    
 }
 
